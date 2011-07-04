798c7e38a46b18b6413f7b7645e8393ba048d83f
Timothy Clare <timothyclare@potix.com>
2011-07-04 19:13:59 +0800

    Removed token

--- src/main/java/me/timothyclare/tawny/dao/api/TokenDao.java
+++
@@ -1,8 +0,0 @@
-package me.timothyclare.tawny.dao.api;
-
-import twitter4j.auth.AccessToken;
-
-public interface TokenDao {
-	AccessToken get(String account);
-	boolean save(String account, AccessToken token);
-}
