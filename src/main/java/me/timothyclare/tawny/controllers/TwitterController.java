948eec71b7e992711596b783c0c42be26b32b17d
Timothy Clare <timothyclare@potix.com>
2011-07-06 09:28:03 +0800

    Removed unnecessary import

--- src/main/java/me/timothyclare/tawny/controllers/TwitterController.java
+++ src/main/java/me/timothyclare/tawny/controllers/TwitterController.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import me.timothyclare.tawny.bean.Tweet;
